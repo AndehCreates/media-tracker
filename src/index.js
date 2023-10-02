b31a446bd772d71fc3bbefe12c2bf8d2cf412ede
Andeh <inevitableandeh@gmail.com>
2023-10-02 12:31:46 -0600

    remove head code in wrong place

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <head>
-      <meta
-        name='viewport'
-        content='initial-scale=1, width=device-width'
-      />
-    </head> */}
     <App />
   </React.StrictMode>
 );
